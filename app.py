502062d69a836340ff703d891283ecefde996597
hpellis <harriet.p.ellis@gmail.com>
2019-02-19 14:21:47 +0000

    updating app routes

--- app.py
+++ app.py
@@ -6,23 +6,21 @@ Created on Mon Feb 18 11:41:13 2019
 @author: harriet
 """
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 import sqlite3
+from engine import Task
 
 
 mock_data=[{"task_id":37,"title":"sorbitol","desc":"Cross-group","status":"Fuscia","date_due":"3/26/2018","date_created":"10/25/2018","imp":"Mauv"},
 {"task_id":46,"title":"Buspirone hydrochloride","desc":"Face to face","status":"Goldenrod","date_due":"8/10/2018","date_created":"7/22/2018","imp":"Orange"},
 {"task_id":64,"title":"Topiramate","desc":"bottom-line","status":"Aquamarine","date_due":"10/27/2018","date_created":"5/30/2018","imp":"Green"},
-{"task_id":73,"title":"Titanium Dioxide","desc":"client-server","status":"Red","date_due":"5/23/2018","date_created":"8/27/2018","imp":"Fuscia"},
-{"task_id":99,"title":"TITANIUM DIOXIDE, OCTINOXATE","desc":"Quality-focused","status":"Green","date_due":"11/12/2018","date_created":"9/28/2018","imp":"Purple"},
-{"task_id":14,"title":"Ceanothus americanus, Hydrastis canadensis, Veratrum album, Anacardium orientale, Argentum nitricum, Condurango, Momordica balsamina, Oxalis acetosella, Pulsatilla, Bile duct, (suis), Colon (suis), Duodenum (suis), Esophagus (suis), Eye (suis), Gallbladder (suis),","desc":"client-server","status":"Blue","date_due":"3/5/2018","date_created":"10/7/2018","imp":"Orange"},
-{"task_id":59,"title":"ANTHOXANTHUM ODORATUM POLLEN, DACTYLIS GLOMERATA POLLEN, LOLIUM PERENNE POLLEN, PHLEUM PRATENSE POLLEN, and POA PRATENSIS POLLEN","desc":"De-engineered","status":"Maroon","date_due":"4/3/2018","date_created":"6/2/2018","imp":"Turquoise"},
-{"task_id":60,"title":"Chlordiazepoxide Hydrochloride","desc":"groupware","status":"Mauv","date_due":"12/21/2018","date_created":"2/15/2019","imp":"Violet"},
-{"task_id":70,"title":"Dicoria canescens","desc":"concept","status":"Teal","date_due":"6/2/2018","date_created":"7/21/2018","imp":"Orange"},
-{"task_id":85,"title":"Bacillus subtilis","desc":"intermediate","status":"Mauv","date_due":"4/22/2018","date_created":"11/28/2018","imp":"Teal"}]
+{"task_id":73,"title":"Titanium Dioxide","desc":"client-server","status":"Red","date_due":"5/23/2018","date_created":"8/27/2018","imp":"Fuscia"},]
+
 
 app = Flask(__name__)
+cors = CORS(app)
 
 
 @app.route('/')
@@ -31,33 +29,68 @@ def practice():
 
 
 @app.route('/mock_data')
-def api_all():
+def mock_data():
     return jsonify(mock_data)
 
 
-@app.route('/api', methods= ['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        dummy_json = {
-                "test":"test"
-                }
-        return jsonify(dummy_data)
+@app.route('/data', methods = ['GET', 'POST'])
+def api():
+    x = Task()
+    result= x.get_all_tasks()
+    return jsonify(result)
     
-    if request.method == 'POST':
-        response = request.values
-        return jsonify(response)
+    
+#loop over objects
+#return as dictionary
+    
+
+@app.route('/addtask', methods = ['GET', 'POST'])
+def create_task():
+    x = Task()
+    task_id = request.form.get('task_id')
+    title = request.form.get('title')
+    desc = request.form.get('title')
+    due_date = request.form.get('title')
+    create_task(task_id, title, desc, due_date)
+    
+
+
+#    def create_task(self, task_id, title, desc=None, due_date):
+#        self.task_id = task_id
+#        self.title = title
+#        self.desc = desc
+##        but optional?
+#        self.status = "to do"
+#        self.due_date = due_date
+#        self.date_created = datetime.now()
+#        self.imp = imp
+#            
+
+#
+#@app.route('/api', methods= ['GET', 'POST'])
+#def index():
+#    if request.method == 'GET':
+#        dummy_json = {
+#                "test":"test"
+#                }
+#        return jsonify(dummy_data)
+#    
+#    if request.method == 'POST':
+#        response = request.values
+#        return jsonify(response)
 
 
 
 
-@app.route('api2', methods = ['GET', 'POST'])
-def prac():
-    if request.method == 'POST':
-        response = request.values.get('key')
-        print(type(request.values))
-        return jsonify({
-                'data': response
-                })
+#@app.route('api2', methods = ['GET', 'POST'])
+#def prac():
+#    if request.method == 'POST':
+#        response = request.values.get('key')
+#        print(type(request.values))
+##        create dictionary of 
+#        return jsonify({
+#                'data': response
+#                })
 
 
 #@app.route('/tasks', methods=['GET', 'POST'])
@@ -78,13 +111,12 @@ def prac():
     
 ##names are the ids for the input fields
     
-#loop over objects
-#return as dictionary
+
     
 
 #@app.route('/tasks/id', methods=['GET', 'PUT', 'DELETE'])
 #def task():
-#    return render_template(f"tasks/{task_id}.html")
+#    pass
 #    
 
     
--- engine.py
+++ engine.py
@@ -6,32 +6,64 @@ Created on Mon Feb 18 11:41:13 2019
 @author: harriet
 """
 from datetime import datetime
+from os.path import exists
+from sqlite3 import connect
 
-class ToDo():
+class Task():
     
     def __init__(self):
+        
+        self.db_path = 'static/database/tasks.db'
+        
+#        self.task_id = task_id
+#        self.title = title
+#        self.desc = desc
+#        self.status = status
+#        self.due_date = due_date
+#        self.date_created = datetime.now()
+#        self.imp = imp
+     
+        
+    def check_db(self):
+        if exists(self.db_path):
+            return True
+        else:
+            return False
+ 
+    
+    def connect_db(self):
+        if self.check_db():
+            try:
+                self.connection = connect(self.db_path)
+                self.c = self.connection.cursor()
+            except Exception as e:
+                print(e)
+        else:
+            print('Database path does not exist.')
+            
+    
+    def query(self, query):
+        try:
+            self.connect_db()
+            self.c.execute(query)
+            self.results = self.c.fetchall()
+            self.connection.close()
+            return self.results
+        except Exception as e:
+            print(e)
+            
+   
+    def get_all_tasks(self):
+        try:
+            query = "SELECT * from tasks;"
+            self.query(query)
+            return self.results
+        except Exception as e:
+            print(e)
+        
+        
+    def create_task(self, task_id, title, desc=None, due_date):
         self.task_id = task_id
-        self.title = title
-        self.desc = desc
-        self.status = status
-        self.due_date = due_date
-        self.date_created = datetime.now()
-        self.imp = imp
-        
-        
-    
-    
-    def create_task(self):
-        pass
-        
-    def update_task(self):
-        pass
-        
-    def complete_task(self):
-        pass
-    
-    def create_task(self):
-        self.task_id = 
         self.title = title
         self.desc = desc
 #        but optional?
@@ -40,8 +72,21 @@ class ToDo():
         self.date_created = datetime.now()
         self.imp = imp
         
+    def update_task(self):
+        pass
         
+    def complete_task(self):
+        pass
+    
+#    def create_task(self):
 
+#        
+        
+x = Task()
+
+result = x.get_all_tasks()
+
+print(result)
     
 #self.creation_date = datetime.now()
     
@@ -56,7 +101,3 @@ class ToDo():
 #    
 #    
 #    
-    
-class Tasks():
-    
-    create 
